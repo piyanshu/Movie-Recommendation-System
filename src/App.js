45442f8164a1b6da62291e27f4a239c8cbba93dd
piyanshu <piyanshumangla456.com>
2023-06-30 23:35:12 +0530

    adding watch functionality"

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import Home from "./home/Home";
-import './app.scss';
-
-const App = () => {
-  return <div>
-    <Home/>
-  </div>
-}
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Home from "./pages/home/Home";
+import Watch from "./pages/watch/Watch";
+import './app.scss';
+
+const App = () => {
+  return <div>
+    <Watch/>
+  </div>
+}
+export default App;
